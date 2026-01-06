0983d7cb50d507f652c1571e04736ada6c3a4999
Evorp <75297863+3vorp@users.noreply.github.com>
2026-01-06 12:35:46 -1000

    chore(lint): update eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
+import { defineConfig, globalIgnores } from "eslint/config";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
 
-export default tseslint.config(
+export default defineConfig(
 	eslint.configs.recommended,
 	tseslint.configs.recommended,
 	globalIgnores(["dist/**"]),
