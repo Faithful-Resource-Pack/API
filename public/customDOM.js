7ee8a5d38275c2e96e5d8c9e75232a8d4a6754f1
Evorp <75297863+3vorp@users.noreply.github.com>
2026-02-14 23:48:36 -0500

    feat: remove hideous github banner and add copyright

--- public/customDOM.js
+++ public/customDOM.js
@@ -1,18 +1,7 @@
 /* eslint-disable no-undef */
 
-/**
- * Swagger custom dom for Swagger UI
- * @author TheRolf
- */
-
-const GITHUB_URL = "https://github.com/Faithful-Resource-Pack/API";
-
 document.addEventListener("DOMContentLoaded", () => {
-	// Add custom footer
-	document.body.innerHTML += `<a href="${GITHUB_URL}" target="_blank"\
-		style="position: absolute; top: 0; right: 0;">\
-		<img decoding="async" loading="lazy" width="149" height="149"\
-			src="https://github.blog/wp-content/uploads/2008/12/forkme_right_green_007200.png"\
-			alt="Fork me on GitHub" data-recalc-dims="1" />\
-	</a>`;
+	document.body.innerHTML += `<div class="swagger-ui" style="display: flex; flex-flow: row nowrap; justify-content: center;">
+		<p>© ${new Date().getFullYear()} Faithful Resource Pack</p>
+	</div>`;
 });
