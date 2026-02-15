c1671dd62944c358b34da569a328a48abca3c2b9
Evorp <75297863+3vorp@users.noreply.github.com>
2026-02-15 00:26:13 -0500

    feat: add source link for #29

--- public/customDOM.js
+++ public/customDOM.js
@@ -1,7 +1,20 @@
 /* eslint-disable no-undef */
 
 document.addEventListener("DOMContentLoaded", () => {
-	document.body.innerHTML += `<div class="swagger-ui" style="display: flex; flex-flow: row nowrap; justify-content: center;">
-		<p>© ${new Date().getFullYear()} Faithful Resource Pack</p>
+	document.body.innerHTML += `<div class="swagger-ui" style="display: flex; flex-flow: column nowrap; align-items: center;">
+		<p style="margin-bottom: 4px">
+			This website was made using the
+			<a
+				href="https://tsoa-community.github.io/docs/"
+				target="_blank"
+				rel="noopener noreferrer"
+			>tsoa</a> framework. View the source
+			<a
+				href="https://github.com/Faithful-Resource-Pack/API"
+				target="_blank"
+				rel="noopener noreferrer"
+			>here</a>!
+		</p>
+		<p style="margin-top: 4px; margin-bottom: 24px">© ${new Date().getFullYear()} Faithful Resource Pack</p>
 	</div>`;
 });
