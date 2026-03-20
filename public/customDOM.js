3354b80d1b1c88ca21518fee7b50bf4aaa379ee6
Evorp <75297863+3vorp@users.noreply.github.com>
2026-03-20 19:03:03 -0400

    feat: add top arrow button
    related to #29

--- public/customDOM.js
+++ public/customDOM.js
@@ -1,23 +1,44 @@
 /* eslint-disable no-undef */
 
 document.addEventListener("DOMContentLoaded", () => {
-	document.body.innerHTML += `<div class="swagger-ui" style="display: flex; flex-flow: column nowrap; align-items: center;">
-		<p style="margin-bottom: 4px">
-			This website was made using the
-			<a
-				href="https://tsoa-community.github.io/docs/"
-				target="_blank"
-				rel="noopener noreferrer"
-			>tsoa</a> framework. View the source
-			<a
-				href="https://github.com/Faithful-Resource-Pack/API"
-				target="_blank"
-				rel="noopener noreferrer"
-			>here</a>!
-		</p>
-		<p style="margin-top: 4px; margin-bottom: 24px">© ${new Date().getFullYear()} Faithful Resource Pack</p>
-	</div>`;
+	document.body.innerHTML += `
+		<div class="swagger-ui" style="display: flex; flex-flow: column nowrap; align-items: center;">
+			<p style="margin-bottom: 4px">
+				This website was made using the
+				<a
+					href="https://tsoa-community.github.io/docs/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>tsoa</a> framework. View the source
+				<a
+					href="https://github.com/Faithful-Resource-Pack/API"
+					target="_blank"
+					rel="noopener noreferrer"
+				>here</a>!
+			</p>
+			<p style="margin-top: 4px; margin-bottom: 24px">© ${new Date().getFullYear()} Faithful Resource Pack</p>
+		</div>
+	`;
 
 	// less terrible responsiveness
 	document.head.innerHTML += `<meta name="viewport" content="width=device-width, initial-scale=1.0" />`;
+
+	// add to top button
+	document.body.innerHTML += `
+		<button
+			type="button"
+			class="go-up-btn hide"
+			onclick="window.scrollTo({ top: 0, behavior: 'smooth' })"
+			title="Return to page start"
+		>
+			⌃
+		</button>
+	`;
+
+	// there is almost certainly a more efficient way to do this
+	window.addEventListener("scroll", () => {
+		const btn = document.querySelector(".go-up-btn");
+		if (window.scrollY > 500) btn.classList.remove("hide");
+		else btn.classList.add("hide");
+	});
 });
